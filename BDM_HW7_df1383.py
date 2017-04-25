9814c899b011a93b20856f891b4efbafeb329f2d
Danny Fay <fay.d@husky.neu.edu>
2017-04-24 21:03:07 -0400

    sql context

--- BDM_HW7_df1383.py
+++ BDM_HW7_df1383.py
@@ -1,6 +1,6 @@
 import csv
 from geopy.distance import vincenty
-from pyspark.sql import SQLContext, Row
+from pyspark.sql import sqlContext, Row
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark import SparkContext
